6df0f434cea5358851a860e4556977282fce4ec7
aloisdg <aloisdegouvello@live.fr>
2022-07-14 17:58:43 +0200

    improve pwa

--- sw.js
+++ sw.js
@@ -2,23 +2,23 @@ self.importScripts("data/games.js");
 
 const cacheName = "alea-v1";
 const contentToCache = [
-  "/",
-  "/index.html",
-  "/app.js",
-  "/cloud-download.svg",
-  "/crown-coin.svg",
-  "/d100.svg",
-  "/d10.svg",
-  "/d12.svg",
-  "/d20.svg",
-  "/d3.svg",
-  "/d4.svg",
-  "/d6.svg",
-  "/d8.svg",
-  "/lib.js",
-  "/style.css",
-  "/sw.js",
-  "/water.css",  
+  "/alea/",
+  "/alea/index.html",
+  "/alea/app.js",
+  "/alea/cloud-download.svg",
+  "/alea/crown-coin.svg",
+  "/alea/d100.svg",
+  "/alea/d10.svg",
+  "/alea/d12.svg",
+  "/alea/d20.svg",
+  "/alea/d3.svg",
+  "/alea/d4.svg",
+  "/alea/d6.svg",
+  "/alea/d8.svg",
+  "/alea/lib.js",
+  "/alea/style.css",
+  "/alea/sw.js",
+  "/alea/water.css",  
 ];
 
 self.addEventListener("install", (e) => {
@@ -32,19 +32,19 @@ self.addEventListener("install", (e) => {
   );
 });
 
-// self.addEventListener("fetch", (e) => {
-//   e.respondWith(
-//     (async () => {
-//       const r = await caches.match(e.request);
-//       console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
-//       if (r) {
-//         return r;
-//       }
-//       const response = await fetch(e.request);
-//       const cache = await caches.open(cacheName);
-//       console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
-//       cache.put(e.request, response.clone());
-//       return response;
-//     })()
-//   );
-// });
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    (async () => {
+      const r = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (r) {
+        return r;
+      }
+      const response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })()
+  );
+});
